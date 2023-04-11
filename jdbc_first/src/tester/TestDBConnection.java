e6496df41a5c838f6392ba52d6962ff0c7096c47
rucha-limbekar <ruchalimbekar76@gmail.com>
2023-04-11 10:24:02 +0530

    Established DB connection

---
+++ jdbc_first/src/tester/TestDBConnection.java
@@ -0,0 +1,25 @@
+package tester;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestDBConnection {
+
+	public static void main(String[] args) {
+		
+		try {
+		// Load mysql connector.(JDBC driver class in method area) : Optional Step
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/dac22?useSSL=false&allowPublicKeyRetrieval=true";
+		//API of java.sql.Driver<Manager class
+		//public static Connection getConnection(url,uname,pwd) throws SQLException.
+		try(Connection cn=DriverManager.getConnection(url, "root", "root")){
+			System.out.println("DB connection successfull !!!!!" +cn);//cn gives impl class name of Connection interface.
+		}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
