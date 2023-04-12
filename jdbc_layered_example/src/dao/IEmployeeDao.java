11d1915b34dc533f6f6bb81cc8e801160dd40291
rucha-limbekar <ruchalimbekar76@gmail.com>
2023-04-12 09:04:36 +0530

    added DAO I/F for classic seperation between What is to be done and How it is to be done

---
+++ jdbc_layered_example/src/dao/IEmployeeDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+import pojos.Employee;
+
+//tester will call the methods included in DAO I/F
+public interface IEmployeeDao {
+
+	/*
+	 * Display details(id,name,salary,join date) of all emps from a specific dept ,
+	 * joined between strt date n end date sql : select id,name,salary,join_date
+	 * from my_emp where deptid=? and join_date between ? and ?
+	 */
+	//add a method declaration for getting selected emps
+	List<Employee> getSelectedEmpDetails(String deptId, Date beginDate, Date endDate) throws SQLException;
+
+}
